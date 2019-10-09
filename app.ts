26ae96210880625a0e2b97579cd80305fe021a42
LenaNguyen27 <nguyen.lena27@hotmail.com>
2019-10-08 20:50:07 -0400

    add cors

--- app.ts
+++ app.ts
@@ -2,11 +2,21 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var cors = require('cors');
 var lessMiddleware = require('less-middleware');
 var logger = require('morgan');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 var indexRouter = require('./routes/index.ts');
 var usersRouter = require('./routes/users.ts');
+const filesRouter = require('./routes/files.ts');
+
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+}
 
 var app = express();
 
@@ -20,17 +30,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/upload', filesRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
